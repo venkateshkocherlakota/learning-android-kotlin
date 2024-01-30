2879144fef96ddfde00bd999ad49ac4cb85adb3a
venki <kocherlakotavenkatesh666@gmail.com>
2024-01-30 11:50:02 -0800

    Task Completed

---
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskManager()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskManager(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            TaskImage(modifier)
+            TaskStatusText(modifier)
+            ComplimentText(modifier)
+        }
+    }
+}
+
+@Composable
+fun ComplimentText(modifier: Modifier) {
+    Text(
+        text = stringResource(R.string.compliment),
+        fontSize = 16.sp
+    )
+}
+
+@Composable
+fun TaskStatusText(modifier: Modifier) {
+    Text(
+        text = stringResource(id = R.string.all_tasks_completed),
+        modifier = modifier.padding( top = 24.dp, bottom = 8.dp),
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun TaskImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id =R.drawable.ic_task_completed),
+        contentDescription = stringResource(R.string.all_tasks_completed)
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TaskManagerPreview() {
+    TaskManagerTheme {
+        TaskManager()
+    }
+}
